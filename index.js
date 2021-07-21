03d57d9aee63561d34ad1665fbfb7f7fdcb2d969
Chhavi <Chhavi.choudhary75@gmail.com>
2021-07-21 23:42:56 +0200

    js functions

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ function australia(userAnswer) {
   // what is the capital of australia?
   // 100 pts
   const isCorrect = userAnswer === 'Canberra';
-  
+
   if (isCorrect) {
-    points += 100; 
+    points += 100;
   }
   return isCorrect;
 }
@@ -14,9 +14,9 @@ function australia(userAnswer) {
 function canada(userAnswer) {
   // what is the capital of canada?
   // 100 pts
-  const isCorrect= userAnswer === 'Ottawa';
-  if(isCorrect){
-    points+=100;
+  const isCorrect = userAnswer === 'Ottawa';
+  if (isCorrect) {
+    points += 100;
   }
   return isCorrect;
 }
@@ -24,44 +24,74 @@ function canada(userAnswer) {
 function uppercase(userAnswer, word) {
   // what is <word> in all capital letters?
   // 200 pts
-  
+  const isCorrect= userAnswer === word.toUpperCase();
+  if(isCorrect){
+    points += 200
+  }
+return isCorrect;
 }
 
 function firstThreeLetters(userAnswer, word) {
   // what are the first three letters of <word>?
   // 200 pts
-  
+const isCorrect= userAnswer=== word.substr(0,3);
+if(isCorrect){
+  points+=200;
+}
+return(isCorrect)
 }
 
 function squared(userAnswer, number) {
   // what is <number> squared?
   // 200 pts
-  
+
   const isCorrect = userAnswer == number ** 2;
+  //parseInt(userAnswer)===number**2
+  if(isCorrect){
+    points+=200;
+    }
   
+return(isCorrect)
 }
 
 function multiplication(userAnswer, num1, num2) {
   // what is <num1> multiplied by <num2>?
   // 300 pts
-  
+  const isCorrect=userAnswer==num1*num2;
+  if(isCorrect){
+    points+=300;
+  }
+return(isCorrect);
 }
 
 function age(userAnswer, currentYear, birthYear) {
   // if someone was born in <birthYear> and already has had their birthday this year, how old are they?
   // 300 pts
-
+const isCorrect=userAnswer == currentYear-birthYear;
+if(isCorrect){
+  points+=300;
+}
+return(isCorrect)
 }
 
 function larger(userAnswer, num1, num2) {
   // which of ${number4} and ${number5} is larger (if they're the same then pick that number)?
   // 300 pts
-  
+  let isCorrect;
+  if(num1>num2){
+    isCorrect=userAnswer==num1;
+  }else{
+    isCorrect=userAnswer==num2;
+  }
+if(isCorrect){
+  points+=300;
+}
+return isCorrect;
 }
 
 function getScore() {
   // returns the current user's quiz score score
-
+return points;
 }
 
 // DO NOT MODIFY CODE UNDER THIS COMMENT
@@ -70,7 +100,7 @@ function getScore() {
 // of what makes this page works. You'l learn about how these
 // things work as you work through the workshop
 
-(function() {
+(function () {
   const words = [
     "squeeze",
     "suspend",
@@ -105,7 +135,7 @@ function getScore() {
   ];
   let currentQuestion = 0;
   const text = document.getElementById("text")
-  
+
   function validate(userResponse) {
     const answer = process[currentQuestion].validator.apply(window, [userResponse, ...(process[currentQuestion].meta ? process[currentQuestion].meta : [])]);
     let response = "";
@@ -120,12 +150,12 @@ function getScore() {
 
     alert(response);
     currentQuestion++;
-    
+
     if (currentQuestion >= process.length) {
       document.getElementById("container").innerHTML = `<h1>Congrats! You finished. You got ${getScore()} points!</h1>`;
       return;
     }
-    
+
     text.innerText = process[currentQuestion].question;
   }
 
